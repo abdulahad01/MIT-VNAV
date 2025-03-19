76c74808e1ce003cf43e328a3e475b7f43691994
abdulahad01 <abdudeturk@gmail.com>
2025-03-19 21:23:37 +0530

    initial commit with lab-1

---
+++ lab_1/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "my_lib.h"
+
+int main()
+{
+    std::cout << "Hello world" ;
+    std::cout << my_lib_fn() ;
+}
---
+++ lab_1/my_lib.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include "my_lib.h"
+
+std::string my_lib_fn(){
+    return "In library function";
+}
